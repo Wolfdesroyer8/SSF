d7f1a51403a659d0c5b23d4834bd7c4e313ff5e9
n-ovux <julianmcclelland3@gmail.com>
2024-01-14 16:47:26 -0800

    added the ability to change direction of motor on jetson side

--- jetson_motor_driver/jetson_motor_driver.py
+++ jetson_motor_driver/jetson_motor_driver.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import serial
 
 Nano = serial.Serial(port="/dev/ttyUSB0", baudrate=9600, timeout=1)
@@ -12,20 +13,79 @@ def serial_read():
     return Nano.readline().decode().strip()
 
 
-while True:
-    print("Attempting connect to Nano")
-    serial_write("p")
-    if serial_read() == "Pong!":
-        break
-    time.sleep(1)
-print("Connected to Nano")
-for i in range(30, 101, 1):
-    serial_write(f"s {i/100}")
-    print(serial_read())
-    serial_write("r")
-    print(serial_read())
-    time.sleep(0.08)
-time.sleep(4)
+def run_motors():
+    # ramp up speed while printing speed
+    for i in range(30, 101, 1):
+        serial_write(f"s {i/100}")
+        speed = serial_read()
+        time.sleep(0.5)
+        serial_write("r")
+        rpm = serial_read()
+        print(speed, rpm, end="\r")
+    print("")
+    for i in range(0, 40, 1):
+        serial_write("r")
+        print(serial_read(), end="\r")
+        time.sleep(0.1)
+    print("")
 
-serial_write("s 0")
-print(serial_read())
+    serial_write("s 0")
+    print(serial_read())
+
+
+def conf_motor_dirs():
+    input("Press [enter] to start the motor")
+    serial_write("s 0.5")
+    serial_read()
+    while True:
+        print("\t[1] Change motor direction")
+        print("\t[2] exit")
+        choice = input()
+        match choice:
+            case "1":
+                serial_write("s 0")
+                serial_read()
+                serial_write("t")
+                while serial_read() == "ERROR: Stop motor before changing direction":
+                    serial_write("t")
+                    time.sleep(0.6)
+                serial_write("s 0.5")
+                serial_read()
+                print("Direction Changed")
+            case "2":
+                serial_write("s 0")
+                serial_read()
+                return
+            case _:
+                print("ERROR: Invalid input")
+
+
+def main():
+    while True:
+        print("Attempting connect to Nano")
+        serial_write("p")
+        if serial_read() == "Pong!":
+            break
+        time.sleep(1)
+    print("Connected to Nano")
+
+    close = False
+    while not close:
+        print("What would you like to do?")
+        print("\t[1] Configure motor directions")
+        print("\t[2] Run motors")
+        print("\t[3] Exit")
+        choice = input()
+
+        match choice:
+            case "1":
+                conf_motor_dirs()
+            case "2":
+                run_motors()
+            case "3":
+                sys.exit()
+            case _:
+                print("ERROR: Invalid input")
+
+
+main()
